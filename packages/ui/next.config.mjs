ba37de7f9c2a6cc4200fe1d2be0105da50a8ab20
Yuktarth Naman <yuktarthnaman@gmail.com>
2026-09-09 13:44:47 +0530

    fix(ui): move typescript and types to dependencies for vercel build

--- packages/ui/next.config.mjs
+++ packages/ui/next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 const nextConfig = {
     reactStrictMode: true,
     basePath: '/pro',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     webpack: (config) => {
         config.resolve.alias.canvas = false;
         config.resolve.alias['@'] = path.resolve(__dirname, 'src');
